d458ef64f466af12387a31820d7234864d48a924
Leonardo Amorim Ramos <leoamorimr@gmail.com>
2022-01-26 19:48:43 +0000

    Inclusion of Customer email verification. It doesn't permit the inclusion of a Customer with an existent email in db.

--- customer/src/main/java/com/leoamorimr/customer/CustomerService.java
+++ customer/src/main/java/com/leoamorimr/customer/CustomerService.java
@@ -1,9 +1,11 @@
 package com.leoamorimr.customer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
@@ -14,9 +16,16 @@ public class CustomerService {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder().firstName(request.firstName()).lastName(request.lastName()).email(request.email()).build();
         //  todo: check if email is valid
+
         //  todo: check if email not taken
+        Customer customerByEmail = customerRepository.findCustomerByEmail(customer.getEmail());
+        if(customerByEmail != null) {
+            log.error("Email already exists! {}",customerByEmail.getEmail());
+            throw new IllegalStateException("Customer email already exists.");
+        }
+
         customerRepository.saveAndFlush(customer); //to have access to customerId
-        //  todo: check if is froudster
+
         FroudCheckResponse froudCheckResponse = restTemplate.getForObject(
                 "http://FROUD/api/v1/froud-check/{customerId}",
                 FroudCheckResponse.class,
